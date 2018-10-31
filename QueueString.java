37f4675167ffa8bb4345d75c3d0681e3a6bacfea
abrarakbar <43749992+abrarakbar@users.noreply.github.com>
2018-10-31 22:56:01 +0700

    Update QueueString.java

--- QueueString.java
+++ QueueString.java
@@ -3,23 +3,23 @@ import java.util.ArrayList;
 
 public class QueueString {
 	ArrayList<String> kelase = new ArrayList<String>();
-	int ismail = -1;
+	int abrar = -1;
 
 	public void insert(String value) {
 		ismail++;
-		kelase.add(ismail,value);
+		kelase.add(abrar,value);
 	}
 	public String get() {
 		String value = "KOSONG";
-		if(ismail>-1) {
+		if(abrar>-1) {
 			value = kelase.get(0);
 			kelase.remove(0);
-			ismail--;
+			abrar--;
 		}
 		return value;
 		
 	}
 	public void cetak() {
-		System.out.println("ECOR : "+ismail+" > KELAS A: "+kelase.toString());
+		System.out.println("ECOR : "+abrar+" > KELAS A: "+kelase.toString());
 	}
 }
